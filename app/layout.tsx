a98d332e5ba571c060788becce897f84fd5115dc
kane.nguyen <khoanguyenvietmanh@gmail.com>
2024-09-22 21:33:33 +0700

    add login UI

---
+++ app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Nunito } from "next/font/google"
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/providers/ToasterProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import toaster, { Toaster } from 'react-hot-toast'
+
+const ToasterProvider = () => {
+    return (
+        <Toaster />
+    )
+}
+
+export default ToasterProvider
